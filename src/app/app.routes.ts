ec79c1ada779c24958bdad51475332537bc3e7e8
Prem Prakash <iamprem021@gmail.com>
2024-03-26 17:29:56 +0530

    error

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./public/public.module').then((m) => m.PublicModule),
+  },
+];
